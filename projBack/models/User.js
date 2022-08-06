790f97d0558fe44d7f4ab6486bd68015c9f0c2ea
Lkhagva-erdene <anidas.com@gmail.com>
2022-08-06 11:46:41 +0800

    Add files via upload

---
+++ projBack/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  question: {
+    type: String,
+    required: true,
+  },
+  answer: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
---
+++ project/src/service/fetchService.js
@@ -0,0 +1,32 @@
+const loginService = async (credentials) => {
+  return fetch("http://localhost:4000/app/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
+};
+const registerService = async (credentials) => {
+  return fetch("http://localhost:4000/app/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
+};
+const pwdReset = async (credentials) => {
+  return fetch("http://localhost:4000/app/forgotpwd", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
+};
+export const services = {
+  registerService,
+  loginService,
+  pwdReset,
+};
